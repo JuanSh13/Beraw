0daec6ee77abd20e2a0edc58948757ac27b41a77
shjuanmsh <shjuanmsh@gmail.com>
2024-04-09 09:56:59 -0600

    aside navigation

--- js/script.js
+++ js/script.js
@@ -1,21 +1,36 @@
+(function() {
+	var $body = document.body;
+  var $menu_trigger = $body.getElementsByClassName('bars-icon')[0];
+
+  console.log($menu_trigger);
+
+	if ( typeof $menu_trigger !== 'undefined' ) {
+		$menu_trigger.addEventListener('click', function() {
+      console.log($body.className)
+			$body.className = ( $body.className == 'menu-active' )? '' : 'menu-active';
+		});
+	}
+
+}).call(this);
+
 // Barra de Navegacion Mobile 
-const barsIcon = document.querySelector('.bars-icon');
-const asideNavigation = document.querySelector('.aside-navigation');
+// const barsIcon = document.querySelector('.bars-icon');
+// const asideNavigation = document.querySelector('.aside-navigation');
 
-let active = false;
+// let active = false;
 
-barsIcon.addEventListener('click', () => {
+// barsIcon.addEventListener('click', () => {
 
-  if(!active) {
-    asideNavigation.style.display = "flex";
-    active = !active;
-  }
-  else {
-    asideNavigation.style.display = "none";
-    active = !active;
-  }
+//   if(!active) {
+//     asideNavigation.style.display = "flex";
+//     active = !active;
+//   }
+//   else {
+//     asideNavigation.style.display = "none";
+//     active = !active;
+//   }
 
-});
+// });
 
 let applicationsNumber =  document.querySelector('.applications-number');
 let clientsNumber =  document.querySelector('.clients-number');
